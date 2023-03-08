f4e5e28494e123291da91fa5b174641526d39591
iitiansuyash <suyashsuryavanshi1111@gmail.com>
2023-03-08 12:31:25 +0530

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Footer.module.css';
+
+const Footer = () => {
+    return ( <>
+        <h1 className={styles.footer}>Footer it is</h1>
+    </> );
+}
+ 
+export default Footer;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+const Navbar = () => {
+    return ( <>
+        <h1>Navbar it is</h1>
+    </> );
+}
+ 
+export default Navbar;
