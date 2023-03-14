e885f16e260cbe0dd85ecc0ce4e03e430102d08f
Samudra <samudramitra2017@gmail.com>
2023-03-15 01:15:51 +0530

    fetched data from backend

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -1,26 +1,8 @@
 import React from "react";
 import styles from "../styles/Team.module.css";
 const TeamCard = ({ user }) => {
-  //   const main = document.querySelector("#main");
-
-  //   for (user of users) {
-  //     newCard(user);
-  //   }
-
-  //   function newCard(user) {
-  //     main.innerHTML += `
-  //    <div class="card">
-  //       <img class="image" src="${user.image}" alt="user image via placeholder">
-  //       <h1 class="title">${user.name}, ${user.age}</h1>
-  //       <p class="text">${user.info}</p>
-  //    </div>
-  //    `;
-  //   }
-
   return (
     <div>
-      {/* <h1 className={styles.ttl}>Our Team</h1> */}
-
       <div className={styles.card}>
         <img
           className={styles.image}
@@ -28,7 +10,7 @@ const TeamCard = ({ user }) => {
           alt="user image via placeholder"
         />
         <h1 className={styles.title}>{user?.name}</h1>
-        <p className={styles.text}>{user?.info}</p>
+        <p className={styles.text}>{user?.position}</p>
       </div>
     </div>
   );
--- src/pages/team.js
+++ src/pages/team.js
@@ -1,64 +1,79 @@
 import Navbar from "@/components/Navbar";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import TeamCard from "../components/TeamCard";
 import styles from "../styles/Team.module.css";
 const Team = () => {
+  const BACKEND_URL = "http://localhost:8000/organizing";
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fun = async () => {
+      const res = await axios.get(BACKEND_URL);
+      // users = res.data;
+      setUsers(res.data);
+
+      // console.log(users);
+    };
+    fun();
+  }, []);
   // const users = await
-  const users = [
-    {
-      name: "Aditya Kumar Garg",
-      age: 22,
-      info: "Coordinator",
-      image: "https://via.placeholder.com/140",
-    },
-    {
-      name: "Anshuman Tejaswi",
-      age: 21,
-      info: "Treasurer",
-      image: "https://via.placeholder.com/140",
-    },
-    {
-      name: "Akshat Singh",
-      age: 24,
-      info: "Sponsorship and PR Coordinator",
-      image: "https://via.placeholder.com/140",
-    },
-    {
-      name: "Akash Mahapatra",
-      age: 21,
-      info: "Operations Coordinator",
-      image: "https://via.placeholder.com/140",
-    },
-    {
-      name: "Aditya Mishra",
-      age: 24,
-      info: "Tech Coordinator",
-      image: "https://via.placeholder.com/140",
-    },
-    {
-      name: "Shulabh Prakash",
-      age: 24,
-      info: "Graphics and Animation Coordinator",
-      image: "https://via.placeholder.com/140",
-    },
-    {
-      name: "Shirin Kaul",
-      age: 27,
-      info: "Content Coordinator",
-      image: "https://via.placeholder.com/140",
-    },
-    {
-      name: "Bhavya Jain",
-      age: 27,
-      info: "Promotion and Marketing Coordinator",
-      image: "https://via.placeholder.com/140",
-    },
-    {
-      name: "Brajesh Meena",
-      age: 27,
-      info: "Promotion and Marketing Coordinator",
-      image: "https://via.placeholder.com/140",
-    },
-  ];
+
+  // [
+  //   {
+  //     name: "Aditya Kumar Garg",
+  //     age: 22,
+  //     info: "Coordinator",
+  //     image: "https://via.placeholder.com/140",
+  //   },
+  //   {
+  //     name: "Anshuman Tejaswi",
+  //     age: 21,
+  //     info: "Treasurer",
+  //     image: "https://via.placeholder.com/140",
+  //   },
+  //   {
+  //     name: "Akshat Singh",
+  //     age: 24,
+  //     info: "Sponsorship and PR Coordinator",
+  //     image: "https://via.placeholder.com/140",
+  //   },
+  //   {
+  //     name: "Akash Mahapatra",
+  //     age: 21,
+  //     info: "Operations Coordinator",
+  //     image: "https://via.placeholder.com/140",
+  //   },
+  //   {
+  //     name: "Aditya Mishra",
+  //     age: 24,
+  //     info: "Tech Coordinator",
+  //     image: "https://via.placeholder.com/140",
+  //   },
+  //   {
+  //     name: "Shulabh Prakash",
+  //     age: 24,
+  //     info: "Graphics and Animation Coordinator",
+  //     image: "https://via.placeholder.com/140",
+  //   },
+  //   {
+  //     name: "Shirin Kaul",
+  //     age: 27,
+  //     info: "Content Coordinator",
+  //     image: "https://via.placeholder.com/140",
+  //   },
+  //   {
+  //     name: "Bhavya Jain",
+  //     age: 27,
+  //     info: "Promotion and Marketing Coordinator",
+  //     image: "https://via.placeholder.com/140",
+  //   },
+  //   {
+  //     name: "Brajesh Meena",
+  //     age: 27,
+  //     info: "Promotion and Marketing Coordinator",
+  //     image: "https://via.placeholder.com/140",
+  //   },
+  // ];
   return (
     <>
       <Navbar />
