d596ce0f3cb11675dcbb34a0f6925356185b31ca
iitiansuyash <suyashsuryavanshi1111@gmail.com>
2023-03-23 10:21:54 +0530

    Typewriter added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,22 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap/dist/css/bootstrap.css'
 import styles from '@/styles/Home.module.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import About from '@/components/About'
 import Events from '@/components/Events'
-import Faq from '@/components/Faq';
+import Faq from '@/components/Faq'
 import Timeline from '@/components/Timeline'
 import Sponsors from '@/components/Sponsors'
 import Glimpse from '@/components/Carousel'
 import Coordinators from '@/components/Coordinators'
 import Testimonials from '@/components/Testimonials'
 import { useEffect, useRef, useState } from 'react'
-import GLOBE from "vanta/dist/vanta.globe.min"
-import * as THREE from "three"
-import FAQ from '@/components/Faq';
+import GLOBE from 'vanta/dist/vanta.globe.min'
+import * as THREE from 'three'
+import Typewriter from 'typewriter-effect'
 // import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 // const inter = Inter({ subsets: ['latin'] })
@@ -25,16 +25,16 @@ export default function Home() {
   const [vantaEffect, setVantaEffect] = useState(0)
   const vantaRef = useRef(null)
   useEffect(() => {
-    if(!vantaEffect){
+    if (!vantaEffect) {
       setVantaEffect(
         GLOBE({
           el: vantaRef.current,
           THREE,
-          scale: 1.00,
-          scaleMobile: 1.00,
+          scale: 1.0,
+          scaleMobile: 1.0,
           color: 0x9d2b9c,
-          backgroundColor: 0x0
-        })
+          backgroundColor: 0x0,
+        }),
       )
     }
     return () => {
@@ -49,25 +49,36 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar/>
-      <main className={styles.main} ref={vantaRef} style={{ overflowY: "hidden", overflow: "hidden" }}>
+      <Navbar />
+      <main
+        className={styles.main}
+        ref={vantaRef}
+        style={{ overflowY: 'hidden', overflow: 'hidden' }}
+      >
         <div className={styles.container}>
           <h1 className={styles.ttl}>Hackfest 2k23</h1>
-          <h2 className={styles.subttl}>Ideate Innovate Iterate</h2>
+          <h2 className={styles.subttl}>
+            <Typewriter
+              options={{
+                strings: ['Ideate;', 'Innovate;', 'Iterate;'],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h2>
           <h2 className={styles.subttl}>7th to 9th APRIL</h2>
           <h2 className={styles.subttl}>IIT ISM Dhanbad</h2>
         </div>
       </main>
-      <About/>
-      <Glimpse/>
-      <Coordinators/>
-      <Events/>
+      <About />
+      <Glimpse />
+      <Coordinators />
+      <Events />
       {/* <Timeline/> */}
       {/* <Testimonials/> */}
-      <Faq/>
-      <Sponsors/>
-      <Footer/>
-
+      <Faq />
+      <Sponsors />
+      <Footer />
     </>
   )
 }
