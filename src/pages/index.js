734d3cf6c7a64e95bea60b0309eda0419757fa82
iitiansuyash <suyashsuryavanshi1111@gmail.com>
2023-03-29 20:27:57 +0530

    Fixed navigations

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,7 +52,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar />
+      <Navbar team_nav="/#sponsors" team_about="/#about" team_contact="/#contact"  />
       <main
         className={styles.main}
         ref={vantaRef}
